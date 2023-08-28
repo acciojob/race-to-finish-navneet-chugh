efb5735a0934a9596ece2493ccb87075d98d0bc8
navneet-chugh <navneetchugh15@gmail.com>
2023-08-28 21:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+// Array of promises
+const promises = [
+  new Promise((resolve) => setTimeout(() => resolve("Promise 1 resolved"), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve("Promise 2 resolved"), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve("Promise 3 resolved"), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve("Promise 4 resolved"), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve("Promise 5 resolved"), getRandomTime())),
+];
+// Function to get random time between 1 and 5 seconds
+function getRandomTime() {
+  return Math.random() * 4000 + 1000; // Between 1 and 5 seconds in milliseconds
+}
+
+// Using Promise.any() to wait for the first promise to resolve
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    console.error("All promises rejected:", error);
+  });
